4fbb4bee2491d2fd81185f3d49911e77f7f66c76
Miftah <m.miftah.yusriansyah@gmail.com>
2025-06-23 12:35:52 +0700

    Minor fix

--- target/classes/data.sql
+++
@@ -1,4 +0,0 @@
-INSERT INTO product (name, description, price, stock) VALUES 
-('T-Shirt', 'Cotton T-Shirt', 100.00, 50),
-('Jeans', 'Blue Denim Jeans', 200.00, 30),
-('Sneakers', 'Running Sneakers', 300.00, 20);
